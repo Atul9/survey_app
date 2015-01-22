2feaed0e091eeb20ff506f67983e1c1b95b2d56d
Atul9 <atul1bhosale@gmail.com>
2015-01-22 18:35:19 +0530

    Add validations to the User

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,9 @@ class User < ActiveRecord::Base
   belongs_to :role
   has_many :responses
   has_many :options, through: :responses
-
+  validates :name, presence: true
+  validates :name, uniqueness: { case_sensitive: false }
   validates :email, uniqueness: true
-  validates_presence_of :gender
-  #validates gender, presence: :true
   validates :gender, inclusion: { in: %w(M F), message: "%{value} is not valid gender"}
   validates :age, length: { is: 2 }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,9 @@
 
 Role.create(name: "admin")
 Role.create(name: "user")
+User.create(name: 'David',email: 'david@gmail.com',age: 25, gender: 'M', password: 'hello')
+Survey.create(name: 'Code Retreat',survey_type: 'ruby', conducted_on: 2014-10-12, count_people: 20)
+Question.create(description: 'How was the code retreat?', survey_id: 1)
+Option.create(description: 'Good',question_id: 1)
+Option.create(description: 'Bad',question_id: 1)
+Response.create(user_id: 1,option_id: 1)
